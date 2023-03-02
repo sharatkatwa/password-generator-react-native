835f604047e3b4396d62268259265f94aec99eb7
Sharat Katwa <sjkatwa2000@gmail.com>
2023-03-02 18:41:47 +0530

    fix: eslint semicolon error and warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,34 +3,7 @@ module.exports = {
   extends: '@react-native-community',
   rules: {
     'prettier/prettier': ['error', {endOfLine: 'auto'}],
+    semi: 'off',
+    '@typescript-eslint/semi': ['off'],
   },
 }
-
-/*
-module.exports = {
-  extends: ['airbnb', 'prettier', 'plugin:node/recommended'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': ['error', {endOfLine: 'auto'}],
-    'spaced-comment': 'off',
-    'no-console': 'warn',
-    'consistent-return': 'off',
-    'func-names': 'off',
-    'object-shorthand': 'off',
-    'no-process-exit': 'off',
-    'no-param-reassign': 'off',
-    'no-return-await': 'off',
-    'no-underscore-dangle': 'off',
-    'class-methods-use-this': 'off',
-    'prefer-destructuring': ['error', {object: true, array: false}],
-    'no-unused-vars': ['warn', {argsIgnorePattern: 'req|res|next|val'}],
-    'no-empty': 'off',
-    'comma-dangle': 'off',
-    parser: 'babel-eslint',
-    parserOptions: {
-      sourceType: 'module',
-      allowImportExportEverywhere: true,
-    },
-  },
-};
-*/
--- .prettierrc.js
+++ .prettierrc.js
@@ -5,4 +5,4 @@ module.exports = {
   singleQuote: true,
   trailingComma: 'all',
   semi: false,
-};
+}
